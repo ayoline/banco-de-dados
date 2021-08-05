f1c1aa9931cfdaa8b530c378f81f6feafd77c73d
ayoline <ayo@outlook.com.br>
2021-08-05 19:02:10 -0300

    finaliza as alteracoes no app

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # banco_de_dados
 
-App em Flutter, criado para estudar sobre banco de dados.
+App em Flutter, criado para estudar sobre banco de dados. 
+No app foram listados e testados vários comandos utilizando o Banco SQlite.
 
 ## Widgets e packages usados nesse projeto:
 
--- lib/Home.dart
+++ lib/Home.dart
@@ -47,20 +47,80 @@ class _HomeState extends State<Home> {
     List usuarios = await db.rawQuery(sql);
 
     for (var usuario in usuarios) {
-      print("item id: " +
-          usuario["id"].toString() +
-          " nome: " +
-          usuario["nome"] +
-          " idade: " +
-          usuario["idade"].toString());
+      print(
+        "item id: " +
+            usuario["id"].toString() +
+            " nome: " +
+            usuario["nome"] +
+            " idade: " +
+            usuario["idade"].toString(),
+      );
     }
     // print("usuarios: " + usuarios.toString());
+  }
+
+  _recuperarUsuarioPeloId(int id) async {
+    Database db = await _recuperarBancoDados();
+
+    // CRUD => Create, Read, Update, Delete
+    List usuarios = await db.query(
+      "usuarios",
+      columns: ["id", "nome", "idade"],
+      where: "id = ? ",
+      whereArgs: [id],
+    );
+
+    for (var usuario in usuarios) {
+      print(
+        "item id: " +
+            usuario["id"].toString() +
+            " nome: " +
+            usuario["nome"] +
+            " idade: " +
+            usuario["idade"].toString(),
+      );
+    }
+  }
+
+  _excluirUsuario(int id) async {
+    Database db = await _recuperarBancoDados();
+
+    int retorno = await db.delete(
+      "usuarios",
+      where: "id = ? ",
+      whereArgs: [id],
+    );
+
+    print("item qtde removida: $retorno");
+  }
+
+  _atualizarUsuario(int id) async {
+    Database db = await _recuperarBancoDados();
+
+    Map<String, dynamic> dadosUsuario = {
+      "nome": "Joao",
+      "idade": 50,
+    };
+
+    int retorno = await db.update(
+      "usuarios",
+      dadosUsuario,
+      where: "id = ? ",
+      whereArgs: [id],
+    );
+
+    print("item qtde atualizada: $retorno");
   }
 
   @override
   Widget build(BuildContext context) {
     //_salvar();
+    //_recuperarUsuarioPeloId(1);
+    //_excluirUsuario(1);
+    //_listarUsuarios();
+    //_atualizarUsuario(2);
     _listarUsuarios();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Banco de dados'),
